a6e9ae195b1b8c2819d5a7bfeb1b9fb7e8746197
maksym.akulov <maksakulov@hotmail.com>
2020-11-01 16:47:58 +0300

    added moving line

---
+++ Assets/Scripts/LineSpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineSpawn : MonoBehaviour
+{
+    public GameObject line;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Spawn()
+    {
+        while(true)
+        {
+            Instantiate(line, new Vector3(-0.44f, 1.28f, 86.3f), Quaternion.identity);
+            yield return new WaitForSeconds(3f);
+        }
+    }
+}
